885b36451866d14bec88271dc190ddf68059c751
Scarlat Alin <scarlat.alin30@gmail.com>
2024-03-11 16:26:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Ex. 1: Scrieti un program care va calcula valoarea urmatoarei functii pentru x citit de la tastatura
+
+Console.WriteLine("Enter a number: ");
+
+double number = double.Parse(Console.ReadLine());
+
+double y;
+
+if(number <= -2)
+{
+    y = 7 * Math.Pow(2, number);
+}
+else if(number <= 0.5)
+{
+    y = 4 * number - 5;
+}
+else 
+{
+    y = 14 * number - 7;
+}
+
+Console.WriteLine("The value of the function for x = " + number + " is y = " + y);
